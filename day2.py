813e5b4c16e80b3bb5f892e60e6df35d7e821e56
Harsh08112001 <68112461+Harsh08112001@users.noreply.github.com>
2020-07-22 20:41:12 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+#########################################################################
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+#########################################################################
+x = 5
+
+print(x > 3 and x < 10)
+
+# returns True because 5 is greater than 3 AND 5 is less than 10
+x = 5
+
+print(x > 3 or x < 4)
+
+# returns True because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+# returns False because not is used to reverse the result
