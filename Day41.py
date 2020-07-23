de9861d18c7f5de5a288f0e6ce03b83cf0fb4108
Harsh08112001 <68112461+Harsh08112001@users.noreply.github.com>
2020-07-23 19:14:04 +0530

    Add files via upload

---
+++ Day41.py
@@ -0,0 +1,11 @@
+
+str1 = "what we think we become, we are python programmers"
+
+
+print(str1.count("we"))
+
+
+print(str1.count("we",0,15))
+
+
+print(str1.count("i"))
---
+++ Day42.py
@@ -0,0 +1,11 @@
+a="I LOVE PYTHON"
+b="i love java"
+c="I like 123"
+print(" string with upper case") 
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
+print("string with lower case") 
+print(a.islower())
+print(b.islower())
+print(c.islower())
